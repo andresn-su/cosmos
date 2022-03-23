0dd0eed7501ecd85d050de07bfbc33f81e5d9935
Andre da Silva <andre.s.04.get.up@gmail.com>
2022-03-23 16:48:35 -0300

    Created the Http class, who makes api requests using the Fetch API.

---
+++ http.ts
@@ -0,0 +1,73 @@
+export class Http {
+	/**
+	 * @param {string}
+	 */
+	public constructor(
+		public path: string
+	) {}
+
+	/**
+	 * @param  {boolean   = true}
+	 * @param  {object}
+	 * @return {Promise<any>}
+	 */
+	public async get(cors: boolean = true, opts?: object): Promise<any> {
+		let res = await this.exec("get", cors, undefined, opts);
+		return res;
+	}
+
+	/**
+	 * @param  {object}
+	 * @param  {boolean   = true}
+	 * @param  {object}
+	 * @return {Promise<any>}
+	 */
+	public async post(data: object, cors: boolean = true, opts?: object): Promise<any>{
+		let res = await this.exec("post", cors, data, opts);
+		return res;
+	}
+
+	/**
+	 * @param  {object}
+	 * @param  {boolean   = true}
+	 * @param  {object}
+	 * @return {Promise<any>}
+	 */
+	public async put(data: object, cors: boolean = true, opts?: object): Promise<any>{
+		let res = await this.exec("put", cors, data, opts);
+		return res;
+	}
+
+	/**
+	 * @param  {boolean   = true}
+	 * @param  {object}
+	 * @return {Promise<any>}
+	 */
+	public async delete( cors: boolean = true, opts?: object): Promise<any>{
+		let res = await this.exec("delete", cors, undefined, opts);
+		return res;
+	}
+
+	/**
+	 * @param  {string}
+	 * @param  {boolean}
+	 * @param  {object}
+	 * @return {Promise<any>}
+	 */
+	private async exec(method: string, cors: boolean, data?: object, opts?: object): Promise<any> {
+		let options: RequestInit = {
+			method,
+			mode: cors ? 'cors' : 'no-cors',
+			...opts
+		};
+
+		if(data) {
+			options = {
+				...options,
+				body: JSON.stringify(data)
+			};
+		}
+
+		return await fetch(this.path, options).then(res => res.json());
+	}
+}
